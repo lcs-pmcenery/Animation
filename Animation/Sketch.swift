6a1fd3fee65fb194ee655c36a7e9d89b50d9eb86
McEnery Peter <pmcenery@lcs-mba3012.lcs.on.ca>
2017-10-28 11:45:51 -0400

    On a roll! Got the bouncing circle done

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -6,9 +6,14 @@ class Sketch : NSObject {
     //       Therefore, the line immediately below must always be present.
     let canvas : Canvas
     
-    // Position of circle
+    // Position of circle. We need two sets of variables, one for x and one for y
     var x : Int
     
+    var dx : Int
+    
+    var y : Int
+    
+    var dy : Int
     // This function runs once
     override init() {
         
@@ -16,19 +21,47 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = random(from: 0, toButNotIncluding: 501)
         
+        y = random(from: 0, toButNotIncluding: 501)
+        
+        // Set the change value
+        dx = 2
+        
+        dy = 2
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
+        // Clean up from last loop
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(centreX: 250, centreY: 250, width: 500, height: 500)
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        // Change position
+        x += dx
+        
+        y += dy
+        // This makes it bounce off the edges of the canvas
+        if x > 500 {
+            dx = -2
+        }
+        if x < 0 {
+            dx = 2
+        }
+        
+        if y > 500 {
+            dy = -2
+        }
+        
+        if y < 0 {
+            dy = 2
+        }
+        // The ellipse that will stay on screen
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: y, width: 50, height: 50)
+        
+       
+    }
         
     }
-    
-}
