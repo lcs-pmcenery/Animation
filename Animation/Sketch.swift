985cc9d133d7a7ecb7ce6e7d2e74edf7c04beded
McEnery Peter <pmcenery@lcs-mba3012.lcs.on.ca>
2017-11-03 11:29:28 -0400

    Got multiple canon balls done.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,7 +8,8 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
-    
+    var a : Double
+    var y : Int
     // This function runs once
     override init() {
         
@@ -16,8 +17,9 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
-        
+        x = 0
+        y = 0
+        a = -100.0/2500.0
     }
     
     // Runs in a loop, forever, to create the animated effect
@@ -26,9 +28,27 @@ class Sketch : NSObject {
         // Change position
         x += 1
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        //make it quadratic
+        //factored form
         
+        y = Int(a * Double(x) * (Double(x)-100))
+        
+        
+        canvas.translate(byX: x, byY: y)
+        
+        // get rid of borders
+        canvas.drawShapesWithBorders = false
+        
+        
+        for e in stride(from: -100, through: 500, by: 100) {
+            
+        // Draw an ellipse in the middle of the canvas
+        canvas.drawEllipse(centreX: 0, centreY: e, width: 10, height: 10)
+        
+        
+            
+        }
     }
     
 }
+
